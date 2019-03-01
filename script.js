26340cb563b079398b84e8dbdbf8f0983c43ed59
Thanh Quy Vo <quythuong1998@Quys-MacBook-Pro.local>
2019-03-01 23:45:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// (function($) {
+//     "use strict";
+
+//     var $navbar = $("#navbar"),
+//         y_pos = $navbar.offset().top,
+//         height = $navbar.height();
+
+//     $(document).scroll(function() {
+//         var scrollTop = $(this).scrollTop();
+
+//         if (scrollTop > y_pos + height) {
+//             $navbar.addClass("navbar-fixed").animate({
+//                 top: 0
+//             });
+//         } else if (scrollTop <= y_pos) {
+//             $navbar.removeClass("navbar-fixed").clearQueue().animate({
+//                 top: "-48px"
+//             }, 0);
+//         }
+//     }); 
+
+// })(jQuery, undefined);
+
+     new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true
+  });
+
+  //methods
+  fullpage_api.setAllowScrolling(false);
